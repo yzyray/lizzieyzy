d6f23e43ca670df03413b6036b850b2109c4375c
yzyray <348533481@qq.com>
2021-08-30 16:26:18 +0800

    Add option

--- src/main/java/featurecat/lizzie/Lizzie.java
+++ src/main/java/featurecat/lizzie/Lizzie.java
@@ -403,7 +403,12 @@ public class Lizzie {
         leelaz.setResponseUpToDate();
         Lizzie.config.notStartPondering = false;
       } else {
-        leelaz.ponder();
+        if ((Lizzie.config.ponderOnStart || Lizzie.leelaz.isPondering())
+            || !Lizzie.board.getHistory().isEmptyBoard()) {
+          leelaz.ponder();
+        } else {
+          Lizzie.frame.refresh();
+        }
         leelaz.setResponseUpToDate();
       }
     }
