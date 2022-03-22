a61aac6e3e5c7ac576898d9b47c40e7ff1013853
yzyray <348533481@qq.com>
2022-03-22 11:25:13 +0800

    update version

--- src/main/java/featurecat/lizzie/Lizzie.java
+++ src/main/java/featurecat/lizzie/Lizzie.java
@@ -42,8 +42,8 @@ public class Lizzie {
   public static Board board;
   public static Leelaz leelaz;
   public static Leelaz leelaz2;
-  public static String lizzieVersion = "yzy2.4.7";
-  public static String checkVersion = "220129";
+  public static String lizzieVersion = "yzy2.4.8";
+  public static String checkVersion = "220322";
   public static boolean readMode = false;
   private static String[] mainArgs;
   public static EngineManager engineManager;
