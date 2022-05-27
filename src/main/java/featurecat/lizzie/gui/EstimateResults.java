c2d457a8a2cb5eba9e4f8b8bf8db02fc9c2aded2
yzyray <348533481@qq.com>
2022-05-27 08:53:59 +0800

    update

--- src/main/java/featurecat/lizzie/gui/EstimateResults.java
+++ src/main/java/featurecat/lizzie/gui/EstimateResults.java
@@ -209,7 +209,6 @@ public class EstimateResults extends JDialog {
   }
 
   private void setLocNearBoard() {
-    // TODO Auto-generated method stub
     int width = this.getWidth();
     int height = this.getHeight();
     int frameX = Lizzie.frame.getX();
@@ -229,6 +228,15 @@ public class EstimateResults extends JDialog {
       boardX = 0;
       boardY = 0;
       boardLenght = Lizzie.frame.independentMainBoard.getWidth();
+    }
+    if (Lizzie.frame.floatBoard != null
+        && Lizzie.frame.floatBoard.isVisible()
+        && !Lizzie.frame.mainPanel.hasFocus()) {
+      frameX = Lizzie.frame.floatBoard.getX();
+      frameY = Lizzie.frame.floatBoard.getY();
+      boardX = 0;
+      boardY = 0;
+      boardLenght = Lizzie.frame.floatBoard.getWidth();
     }
     if (frameX + boardX + boardLenght + width + 5 <= screenWidth)
       this.setLocation(
